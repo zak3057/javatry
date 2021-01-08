db88a881336cc784eba0ad7d85bda67196779f18
S.YAMAZAKI <syamazaki1678@gmail.com>
2021-01-08 16:51:55 +0900

    Step04MethodTest.java 完了

--- src/test/java/org/docksidestage/javatry/basic/Step04MethodTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step04MethodTest.java
@@ -69,19 +69,21 @@ public class Step04MethodTest extends PlainTestCase {
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_method_object() {
-        St4MutableStage mutable = new St4MutableStage();
+        St4MutableStage mutable = new St4MutableStage(); // インスタンス生成
         int sea = 904;
         boolean land = false;
-        helloMutable(sea - 4, land, mutable);
-        if (!land) {
+        helloMutable(sea - 4, land, mutable); // seaを返しているが受け取っていない
+        if (!land) { // falseの否定で通る
+            // 904 + "mystic".length() -> 904 + 6
             sea = sea + mutable.getStageName().length();
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 910
     }
 
+    // sea -> 900, land -> false, piari -> St4MutableStage インスタンス
     private int helloMutable(int sea, Boolean land, St4MutableStage piari) {
-        sea++;
-        land = true;
+        sea++; // 901
+        land = true; // true
         piari.setStageName("mystic");
         return sea;
     }
@@ -94,7 +96,7 @@ public class Step04MethodTest extends PlainTestCase {
             return stageName;
         }
 
-        public void setStageName(String stageName) {
+        public void setStageName(String stageName) { // mystic
             this.stageName = stageName;
         }
     }
@@ -108,18 +110,18 @@ public class Step04MethodTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_method_instanceVariable() {
         hasAnnualPassport = true;
-        int sea = inParkCount;
+        int sea = inParkCount; // 0
         offAnnualPassport(hasAnnualPassport);
         for (int i = 0; i < 100; i++) {
             goToPark();
-        }
-        ++sea;
-        sea = inParkCount;
-        log(sea); // your answer? => 
+        } // inParkCount -> 100
+        ++sea; // 1
+        sea = inParkCount; // 100
+        log(sea); // your answer? => 100
     }
 
     private void offAnnualPassport(boolean hasAnnualPassport) {
-        hasAnnualPassport = false;
+        hasAnnualPassport = false; // 引数（ローカル変数）に代入しているためグローバルには影響しない
     }
 
     private void goToPark() {
@@ -152,12 +154,48 @@ public class Step04MethodTest extends PlainTestCase {
      */
     public void test_method_making() {
         // comment out after making these methods
-        //String replaced = replaceCtoB(replaceAtoB("ABC"));
-        //String sea = addPrefix("broadway", replaced);
-        //if (isAvailableLogging()) {
-        //    showSea(sea);
-        //}
+        String replaced = replaceCtoB(replaceAtoB("ABC"));
+        String sea = addPrefix("broadway", replaced);
+        if (isAvailableLogging()) {
+            showSea(sea);
+        }
     }
 
     // write methods here
+    /**
+     * o replaceAtoB(): 一つのString引数、引数を "A" を "B" に置き換えらたStringを戻す
+     */
+    private String replaceAtoB(String str) {
+        return str.replace("A", "B");
+    }
+
+    /**
+     * o replaceCtoB(): 一つのString引数、引数を "C" を "B" に置き換えらたStringを戻す
+     */
+    private String replaceCtoB(String str) {
+        return str.replace("C", "B");
+    }
+
+    /**
+     * o addPrefix(): 二つのString引数、第一引数と ":" と第二引数を連結したものを戻す
+     */
+    private String addPrefix(String str1, String str2) {
+        return str1 + ":" + str2;
+    }
+
+    /**
+     * o isAvailableLogging():
+     * 引数なし、privateのインスタンス変数 "availableLogging" (初期値:true) を戻す (それも作る)
+     */
+    private boolean availableLogging = true;
+    private boolean isAvailableLogging() {
+        return availableLogging;
+    }
+
+    /**
+     * o showSea(): 一つのString引数、戻り値なし、引数をlog()で表示する
+     */
+    private void showSea(String str) {
+        log(str);
+    }
 }
